39d8f6046c7f59fab86433e4f3ebdc09d76d86db
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-05-24 16:33:28 -0500

    auto update

--- run.sh
+++ run.sh
@@ -1,14 +1,13 @@
 #! /bin/bash
 set -exu
 [[ $# -eq 0 ]]
+cd "`dirname "$(readlink -f "$0")"`"
 
-cat > /dev/null << "EOF"
-sudo             -- \
-nice -n +20      -- \
-sudo -u `whoami` -- \
-docker-compose build
+command -v docker ||
+curl https://raw.githubusercontent.com/InnovAnon-Inc/repo/master/get-docker.sh | bash
 
-docker push innovanon/abaddon:latest || :
+docker volume inspect abaddonvol ||
+docker volume create  abaddonvol
 
 trap 'docker-compose down' 0
 
@@ -16,30 +15,31 @@ xhost +local:`whoami`
 sudo             -- \
 nice -n +20      -- \
 sudo -u `whoami` -- \
-docker-compose up --force-recreate
-EOF
-
-docker version ||
-dockerd &
-
-docker volume inspect abaddonvol ||
-docker volume create  abaddonvol
-
-xhost +local:`whoami`
-sudo             -- \
-nice -n +20      -- \
-sudo -u `whoami` -- \
-docker-compose up --build
+docker-compose up --build --force-recreate
 #docker-compose up -d --build
 
 #docker run   -t --net=host -e DISPLAY=${DISPLAY} --mount source=abaddonvol,target=/root/oblige --rm --name abaddon innovanon/abaddon --help
 
-# Create but don't run container from resulting image
-CID=$(docker create --mount source=abaddonvol,target=/root/oblige innovanon/abaddon)
+WAD=`hostname`-`date +%Y-%m-%d.%H-%M-%S.%N`.wad
+( # Create but don't run container from resulting image
+  CID=$(docker create --mount source=abaddonvol,target=/root/oblige innovanon/abaddon-gui)
 
-# Container be gone
-trap "docker rm ${CID}" 0
+  # Container be gone
+  trap "docker rm ${CID}" 0
+
+  docker cp ${CID}:/root/oblige/wads/latest.wad $WAD )
 
 # Grab that artifact sweetness
-docker cp ${CID}:/root/oblige/wads/latest.wad `hostname`-`date +%Y-%m-%d.%H-%M-%S.%N`.wad
+for k in *.wad ; do
+[[ $k != $WAD ]]          || continue
+[[ ! `diff -q $k $WAD` ]] || continue
+rm -v $WAD
+break
+done
+
+docker-compose push
+( #git pull
+  git add .
+  git commit -m "auto commit by $0"
+  git push ) || :
 
