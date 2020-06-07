a8fbca4ff03ec24fb3c75bdb4588232350ce7c7f
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-06-07 13:08:24 +0000

    update

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ docker volume create  abaddonvol
 
 trap 'docker-compose down' 0
 
-xhost +local:`whoami`
+xhost +local:`whoami` || :
 sudo             -- \
 nice -n +20      -- \
 sudo -u `whoami` -- \
